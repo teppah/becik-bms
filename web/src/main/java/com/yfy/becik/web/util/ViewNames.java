ee0e135f613d6b3d3cda7855e2a52a812fe797f5
Unknown <yifengyang01@gmail.com>
2019-01-23 14:58:47 -0500

    added view names constants holder

---
+++ web/src/main/java/com/yfy/becik/web/util/ViewNames.java
@@ -0,0 +1,14 @@
+package com.yfy.becik.web.util;
+
+/**
+ * Class that holds view names as constants.
+ * Contains all the names of the Thymeleaf templates under
+ * resources/templates.
+ * */
+public final class ViewNames {
+    private ViewNames(){
+        throw new UnsupportedOperationException("bruh");
+    }
+
+    public static final String HOME = "home";
+}
