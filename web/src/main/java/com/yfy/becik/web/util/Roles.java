8c6c324e54783745b1411d09ea15351cc0c7510b
Unknown <yifengyang01@gmail.com>
2019-01-23 15:18:19 -0500

    added class to hold role names

---
+++ web/src/main/java/com/yfy/becik/web/util/Roles.java
@@ -0,0 +1,12 @@
+package com.yfy.becik.web.util;
+
+/**
+ * Class that holds role name constants for Security.
+ * */
+public final class Roles {
+    private Roles() {
+        throw new UnsupportedOperationException("you can't");
+    }
+
+    public static final String USER = "user";
+}
