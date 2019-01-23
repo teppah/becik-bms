70409b70867577d4a81c989b95fe0543d1b439ed
Unknown <yifengyang01@gmail.com>
2019-01-23 15:18:31 -0500

    refactor view mapping in controller

--- web/src/main/java/com/yfy/becik/web/controller/MainController.java
+++ web/src/main/java/com/yfy/becik/web/controller/MainController.java
@@ -1,5 +1,6 @@
 package com.yfy.becik.web.controller;
 
+import com.yfy.becik.web.util.ViewMappings;
 import com.yfy.becik.web.util.ViewNames;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -7,7 +8,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class MainController {
 
-    @GetMapping("home")
+    @GetMapping(ViewMappings.HOME)
     public String home() {
         return ViewNames.HOME;
     }
