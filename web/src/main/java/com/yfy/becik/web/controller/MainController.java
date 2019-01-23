56236929fbf40fedd4eb8a5bd65c97e88c9b9a8b
Unknown <yifengyang01@gmail.com>
2019-01-23 14:59:10 -0500

    created main controller and set home view

--- web/src/main/java/com/yfy/becik/web/controller/MainController.java
+++ web/src/main/java/com/yfy/becik/web/controller/MainController.java
@@ -1,8 +1,14 @@
 package com.yfy.becik.web.controller;
 
+import com.yfy.becik.web.util.ViewNames;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class MainController {
-    
+
+    @GetMapping("home")
+    public String home() {
+        return ViewNames.HOME;
+    }
 }
